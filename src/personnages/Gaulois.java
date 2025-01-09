a9070d82ecb66d488938e5049c1c443e9d8f62e7
EliamHannaUpssitech <eliam.hanna@etu-tlse3.fr>
2025-01-09 11:01:42 +0100

    Historisation périodique du projet

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,20 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public String parler(String texteAffiche) {
+		return "Le gaulois " + this.nom + " : " + texteAffiche;
+	}
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		System.out.println(asterix.getNom());
+	}
+}
